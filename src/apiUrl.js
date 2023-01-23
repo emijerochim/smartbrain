54fa49b58be56d4f0ca64cf1f48a98b15e52ddbe
emijerochim <emijerochim@gmail.com>
2023-01-22 23:03:38 -0300

    register and login

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -1,4 +1,3 @@
-const apiUrl = "http://localhost:3001";
-//"https://smartbrain-api-production-4344.up.railway.app";
+const apiUrl = "https://smartbrain-api-production-4344.up.railway.app";
 
 export default apiUrl;
