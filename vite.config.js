39b5f95f5420ace5bd5f90cff5a4d323804f9e9f
emijerochim <emijerochim@gmail.com>
2023-02-17 05:08:46 -0300

    build debug

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  hostname: "/smartbrain/"
+  hostname: "/smartbrain/",
+  base: "/smartbrain/",
 });
